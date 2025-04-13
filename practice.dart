1c7aeed02f4bfb9b80c1be44734e79fcb14517fd
abdulhadi291 <abdulhadikamal291@gmail.com>
2025-04-13 13:53:25 +0500

    properties of LISt

--- practice.dart
+++ practice.dart
@@ -20,6 +20,12 @@
 // }
 
 void main(){
-    List stdnames =["Hadi , Ali, Bilal , Umar"];
+    List stdnames =["Hadi" , "Ali", "Bilal" , "Umar"];
+    print(stdnames[3]);
+    print(stdnames.length); // to get the length of the list
+    print(stdnames) ;  
+    stdnames.add("ahad");     // to add in the list
+    stdnames.addAll(["zain" , "shahryar"]); //  to add multiple names in list
+    print(stdnames.length);
     print(stdnames);
 }
