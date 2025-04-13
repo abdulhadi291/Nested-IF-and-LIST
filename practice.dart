6f9155e4ec5f3bbf79e65afde0c92bc3f6f925ea
Jawwad-Abdl-Naffay <jawwadabdulnaffay@gmail.com>
2025-04-13 13:12:21 +0500

    nested if else practice

---
+++ practice.dart
@@ -0,0 +1,20 @@
+void main ()
+{
+    int a =-10;
+    int b =10;
+
+if(a==b){
+    print("a is equal to b");
+
+}else {
+
+    if(true){
+        print("nested if");
+    }
+    else {
+        print("nested ekse");
+
+    }
+    print("a is not equal to b");
+}
+}
